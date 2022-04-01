f8943ab097e77b08546715d08c47a92c0e824ce2
NokonoKotlin <58871792+NokonoKotlin@users.noreply.github.com>
2022-04-01 16:04:52 +0900

    Add files via upload

--- web/test.cpp
+++ web/test.cpp
@@ -1,139 +1,132 @@
+//include
+//------------------------------------------
+#include <vector>
 #include<iostream>
-#include<vector>
-#include<deque>
-#include<queue>
-#include<algorithm>
+#include<math.h>
 #include<map>
-
-
-
-
-const long long INF = 1e18;
 using namespace std;
 
-class UnionFind{
-    vector<int> par;
-    vector<int> rank;
-    int Size;
-    public:
-    UnionFind(int Size_){
-        Size = Size_+10;
-        vector<int>(Size).swap(par);
-        vector<int>(Size,1).swap(rank);
-        for(int i = 0 ; i < Size ; i++){
-            par[i] = i;
-        }
-    }
 
-    int root(int x){
-        if(par[x] == x){
-            return x;
-        }else{
-            return root(par[x]);
-        }
-    }
+#define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
 
 
-    void unite(int a, int b){
-        if(root(a) == root(b))return;
-        int ra = root(a);
-        int rb = root(b);
-        if(rank[ra] > rank[rb]){
-            par[rb] = ra;
-        }else if(rank[rb] > rank[ra]){
-            par[ra] = rb;
-        }else{
-            par[ra] = rb;
-            rank[rb]++;
-        }
-    }
+template<class T>
+class SegTree{
+    private:
 
-    bool same(int a, int b){
-        return root(a) == root(b);
-    }
-
-};
-
-
-
-int N , M ,Q;
-
-struct edge{
-    public:
-    long long  u , v , weight , index;
-
-    edge(){}
+    vector<T> node;
     
-    edge(long long a , long long b , long long c ,long long d){
-        u = a ;
-        v = b;
-        weight = c;
-        index = d;
+
+    public:
+    int Size;//葉のサイズ
+    
+    
+    SegTree(int Size_){
+        
+        init(Size_);
+
     }
+
+    void init(int S){
+        
+        int k = log2(S) + 2;
+
+        vector<T>((1<<k),0).swap(node);
+        Size = (1<<(k-1));//葉のサイズ
+    }
+
+    /*
+        区間に対する演算 ... + , xor や maxなどができる
+    */
+    T merge(T a, T b){
+        //自分で指定..他にも変更すべき場所があるので注意
+        return max(a,b);
+    }
+
+
+    /*
+        葉から根に登りながら更新
+        mergeの種類によって編集しないといけない
+    */
+    void update(int index , T val){
+        if(index < 0 || index >=Size)return;
+
+        index += Size;
+        T prev = node[index];
+        node[index] = val;
+        T dif = val - prev;
+        while(index/2 > 0){
+            node[index/2] = merge(node[(index/2*2)] , node[(index/2)*2+1]);
+            
+            
+            index/=2;
+        }
+
+    }
+
+    /*
+        [lef,rig)の半開区間について,木の頂点:SubRootが覆う区間について調べる。
+
+    */
+    T query(int lef , int rig , int SubRoot = 1 , int SubTreeLeft = 0 , int SubTreeRight = -1){
+        if(SubRoot == 1)SubTreeRight = Size;
+
+        
+        if(rig <= SubTreeLeft){
+            return 0;
+        }else if(lef >=SubTreeRight){
+            return 0;
+        }else if(  lef <= SubTreeLeft &&  SubTreeRight <= rig ){
+            return node[SubRoot];
+        }else{
+            int border = (SubTreeLeft+SubTreeRight)/2;
+            return merge( query(lef,rig,SubRoot*2,SubTreeLeft,border) , query(lef,rig,SubRoot*2+1,border,SubTreeRight) );
+        }
+    }
+
+
 
 };
 
 
-bool sort_compare(edge a, edge b){
-    return a.weight < b.weight;
-}
 
 int main(){
-    cin >> N >> M >> Q;
-
-
-
-
-    vector<edge> E(M+Q);
-
-    //edge.index は、何番目のクエリの辺か(0なら元々グラフにある辺　、x(>=1)ならx番目のクエリの辺)
-    //辺を重みが小さい順に順に見ているとする。
-    //辺eを見ている時、kruskal法に基づいて採用する場合、eが元々グラフ上にあった辺なら頂点同士を連結する。
-    //eがクエリ由来の辺なら、ans[e.index]="Yes"として、辺eは使わない(これ以降に見る元のグラフ由来の辺の採用に影響するから)。
-
-
-    for(int i = 0 ; i<M;i++){
-        long long a , b , c;
-        cin >> a >> b >> c;
-        E[i] = edge(a,b,c,0);
-    }
-
-    for(int i = 0 ; i < Q ; i++){
-        long long a , b , c;
-        cin >> a >> b >> c;
-        E[M+i] = edge(a,b,c,i+1);
+    long long  n , m , k , A , B;    string s;    map<long long  , long long >p;
+    vector<pair<long long ,long long >> Pairs;
+    pair<long long , long long> Ap;
+    cin >> n >> k;
  
-        
-        
+    vector<long long> a(1005000,0);
+    
+    for(int i = 0 ; i < n ; i++)cin>> a[i];
+    long long INF = -1e18;
+    for(long long x:a)INF = max(x,INF);
+    SegTree<long long> S(n);
+
+    vector<long long> dp(n+10,0);
+
+    for(int i = 0 ; i < n ; i++){
+        long long maxi = max(a[i] - k, (long long)0);
+        dp[i] = S.query( maxi , min(a[i] + k + 1 , INF + 1 ) ) + 1;
+       
+        S.update(a[i] , dp[i]);
+        //debug(dp[i])
     }
-
-    //重みが小さい順にソート
-    sort(E.begin(),E.end(),sort_compare);
-
-    UnionFind T(N);
-
-    vector<string> ans(Q,"No");
-
-    for(edge e:E){   
-        //前から見ていく
-
-        //非連結成分同士を繋ぐ辺の場合
-        if(T.same(e.u,e.v) == 0){
-             
-           
-            if(e.index == 0){ 
-                //元々存在する辺の場合、連結する。
-                T.unite(e.u,e.v);
-            }else{
-                //後から追加する辺の場合、判定だけして連結はしない
-                ans[e.index-1] = "Yes" ;
-            }
-        }
-    }
-
-    for(string s:ans)cout << s << endl;
-
-
-
+    
+    long long ans = -1e18;
+    for(long long x:dp)ans = max(ans,x);
+    cout << ans << endl;
+ 
+ 
+ 
+ 
+ 
+ 
     return 0;
 }
+ 
+ 
+ 
+
+
+
