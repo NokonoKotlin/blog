9617933318f7ef45f6c6a1c06f05e2fffc2d62b8
NokonoKotlin <58871792+NokonoKotlin@users.noreply.github.com>
2022-04-01 23:54:29 +0900

    Add files via upload

--- web/test.cpp
+++ web/test.cpp
@@ -10,6 +10,8 @@ using namespace std;
 #define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
 
 
+
+
 template<class T>
 class SegTree{
     private:
@@ -19,28 +21,31 @@ class SegTree{
 
     public:
     int Size;//葉のサイズ
+    T initializer;//配列の初期値
     
+    SegTree(){}
     
-    SegTree(int Size_){
+    SegTree(int Size_ , T initializer_){
         
-        init(Size_);
+        init(Size_,initializer_);
 
     }
 
-    void init(int S){
-        
+    void init(int S , T I){
+        initializer = I;
         int k = log2(S) + 2;
 
-        vector<T>((1<<k),0).swap(node);
+        vector<T>((1<<k),initializer).swap(node);
         Size = (1<<(k-1));//葉のサイズ
     }
+
 
     /*
         区間に対する演算 ... + , xor や maxなどができる
     */
     T merge(T a, T b){
         //自分で指定..他にも変更すべき場所があるので注意
-        return max(a,b);
+        return a+b;
     }
 
 
@@ -56,16 +61,16 @@ class SegTree{
         node[index] = val;
         T dif = val - prev;
         while(index/2 > 0){
-            node[index/2] = merge(node[(index/2*2)] , node[(index/2)*2+1]);
-            
-            
             index/=2;
+            node[index] = merge(node[index*2] , node[index*2+1]);
         }
 
     }
 
+
+
     /*
-        [lef,rig)の半開区間について,木の頂点:SubRootが覆う区間について調べる。
+        [lef,rig)の半開区間について,木の頂点:SubRootが根となる部分木が覆う区間について調べる。
 
     */
     T query(int lef , int rig , int SubRoot = 1 , int SubTreeLeft = 0 , int SubTreeRight = -1){
@@ -73,12 +78,16 @@ class SegTree{
 
         
         if(rig <= SubTreeLeft){
+            //区間が被っていない
             return 0;
         }else if(lef >=SubTreeRight){
+            //区間が被っていない
             return 0;
         }else if(  lef <= SubTreeLeft &&  SubTreeRight <= rig ){
+            //SubRootを根とする部分木の範囲が、取得したい範囲に完全に含まれている-> node[SubRoot]の値をreturn
             return node[SubRoot];
         }else{
+            //SubRootの右の根と左の根に降りて、再度調べる
             int border = (SubTreeLeft+SubTreeRight)/2;
             return merge( query(lef,rig,SubRoot*2,SubTreeLeft,border) , query(lef,rig,SubRoot*2+1,border,SubTreeRight) );
         }
@@ -91,33 +100,20 @@ class SegTree{
 
 
 int main(){
-    long long  n , m , k , A , B;    string s;    map<long long  , long long >p;
-    vector<pair<long long ,long long >> Pairs;
-    pair<long long , long long> Ap;
-    cin >> n >> k;
- 
-    vector<long long> a(1005000,0);
     
-    for(int i = 0 ; i < n ; i++)cin>> a[i];
-    long long INF = -1e18;
-    for(long long x:a)INF = max(x,INF);
-    SegTree<long long> S(n);
-
-    vector<long long> dp(n+10,0);
-
-    for(int i = 0 ; i < n ; i++){
-        long long maxi = max(a[i] - k, (long long)0);
-        dp[i] = S.query( maxi , min(a[i] + k + 1 , INF + 1 ) ) + 1;
-       
-        S.update(a[i] , dp[i]);
-        //debug(dp[i])
+    long long n , q;
+    cin >> n >> q;
+    SegTree<long long> S(n,0);
+    while(q--){
+        int c ;
+        long long x , y;
+        cin >> c >> x >> y;
+        if(c==0){
+            S.update(x,S.query(x,x+1)+y);
+        }else{
+            cout << S.query(x,y+1) << endl;
+        }
     }
-    
-    long long ans = -1e18;
-    for(long long x:dp)ans = max(ans,x);
-    cout << ans << endl;
- 
- 
  
  
  
